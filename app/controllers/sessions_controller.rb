3569445f743f0fd3d83562ca09a30eac21e1b9d2
eihiko <oeihiko@gmail.com>
2016-01-25 17:24:37 -0500

    Add logout to userbar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,16 +19,16 @@ class SessionsController < ApplicationController
     user = User.try_login params[:username], params[:password]
     if user
       session[:user_id] = user.id
-      redirect_to "/", notice: "Welcome back!"
+      redirect_to({controller: "sessions", action: "home"}, notice: "Welcome back!")
     else
-      redirect_to controller: "sessions", action: "login", alert: "User authentication failed."
+      redirect_to({controller: "sessions", action: "login"}, alert: "User authentication failed.")
     end
   end
 
   def logout
     session[:user_id] = nil
     @current_user = nil
-    redirect_to controller: "sessions", action: "login", notice: "Logout complete."
+    redirect_to({controller: "users", action: "new"}, notice: "Logout complete.")
   end
 
 end
