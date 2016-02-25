503729c9ea8e3dd50ca37141fdb2f8e9f68ba74c
Eihiko <oeihiko@gmail.com>
2016-02-25 10:50:12 -0500

    Override authenticity token check

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,7 @@
 class Api::UsersController < ApplicationController
 
   skip_before_action :require_login
+  skip_before_action :verify_authenticity_token
   
   def show
     user = User.find_by_guid(params[:id])
