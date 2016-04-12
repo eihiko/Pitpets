8a26456ca0169de42bbf4ac51a09baedb882fdb6
Eihiko <oeihiko@gmail.com>
2016-04-12 18:43:10 -0400

    Validate usernames for uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   acts_as_messageable
 
   before_save { self.email = email.downcase }
-  validates :username, presence: true, length: { maximum: 50 }
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 16 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_secure_password
