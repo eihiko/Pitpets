30285d1c103a5bc1b027a38abf517f1b9fe24c70
Adam Gleichsner <amg188@case.edu>
2016-04-21 01:07:00 -0400

    Now skips request if there's a request from the target already

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -30,10 +30,18 @@ class FriendsController < ApplicationController
   def send_request
   	current_user = params[:current_user]
   	target_user = params[:target_user]
+  	_request = FriendRequest.find_by(to_user: current_user, from_user: target_user)
 
-  	FriendRequest.create(to_user: target_user, from_user:current_user)
+  	if _request
+  		Friend.create(user_1: _request.to_user, user_2: _request.from_user)
+  		_request.destroy
+  		redirect_to "/friends/find", notice: "Friend Added!"
+  	else
+	  	FriendRequest.create(to_user: target_user, from_user:current_user)
+	  	redirect_to "/friends/find", notice: "Request Sent!"
+  	end
 
-  	redirect_to "/friends/find", notice: "Request Sent!"
+  	
   end
 
   def review
